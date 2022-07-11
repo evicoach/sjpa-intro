921b39ebdc97e0c1c96f2286effc5d2ae9e7bd82
Evi Owhotemu <evicoach@gmail.com>
2022-07-11 06:35:37 +0100

    Added liquibase changelog

--- src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSplice.java
+++ src/test/java/guru/springframework/sdjpaintro/SpringBootJpaTestSplice.java
@@ -9,9 +9,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 import org.springframework.test.annotation.Commit;
-import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
